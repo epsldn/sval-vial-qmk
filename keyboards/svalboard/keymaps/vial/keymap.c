0d9dd4d7c92d4167b3ef74dabbc20013cd3be7fd
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2026-05-04 16:52:27 -0500

    Update keymap.c

--- keyboards/svalboard/keymaps/vial/keymap.c
+++ keyboards/svalboard/keymaps/vial/keymap.c
@@ -1,20 +1,3 @@
-/*
-Copyright 2023 Morgan Venable @_claussen
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #include "../keymap_support.c"
 #include "keycodes.h"
 #include "quantum_keycodes.h"
@@ -47,100 +30,79 @@ enum layer {
 int sval_macro_size = 0;
 uint8_t sval_macros[] = {0};
 const uint16_t PROGMEM keymaps[DYNAMIC_KEYMAP_LAYER_COUNT][MATRIX_ROWS][MATRIX_COLS] = {
-    /* ===== NORMAL ===== .*/
     [NORMAL] = LAYOUT(
-        /*     Center            North               East                South             West                Double*/
-        /*R1*/ KC_J              , KC_U              , KC_QUOTE          , KC_M            , KC_H              , KC_NO           ,
-        /*R2*/ KC_K              , KC_I              , KC_COLON          , KC_COMMA        , KC_Y              , KC_NO             ,
-        /*R3*/ KC_L              , KC_O              , LT(BOARD_CONFIG, KC_NO)  , KC_DOT   , KC_N              , KC_NO           ,
-        /*R4*/ KC_SEMICOLON      , KC_P              , KC_BSLS           , KC_SLASH        , KC_RBRC           , KC_NO          ,
-        /*L1*/ KC_F              , KC_R              , KC_G              , KC_V            , LSFT(KC_QUOTE)    , KC_NO            ,
-        /*L2*/ KC_D              , KC_E              , KC_T              , KC_C            , KC_GRAVE          , KC_NO         ,
-        /*L3*/ KC_S              , KC_W              , KC_B              , KC_X            , KC_ESCAPE         , KC_NO           ,
-        /*L4*/ KC_A              , KC_Q              , KC_LBRC           , KC_Z            , KC_DELETE         , KC_NO           ,
-        
-        /*     Down                 Pad                Up                  Nail            Knuckle             DoubleDown*/
-        /*RT*/ MO(NAVNAS)          , KC_SPACE        , KC_NO             , KC_BSPC         , KC_LALT           , MO(FUNC)             ,
-        /*LT*/ KC_LSFT       , LT(NAVNAS, KC_ENTER)  , KC_NO             , LGUI_T(KC_TAB)  , KC_LCTL           , KC_CAPS       
-        ),
+        KC_J, KC_U, KC_QUOTE, KC_M, KC_H, KC_NO,
+        KC_K, KC_I, KC_COLON, KC_COMMA, KC_Y, KC_NO,
+        KC_L, KC_O, LT(BOARD_CONFIG, KC_NO), KC_DOT, KC_N, KC_NO,
+        KC_SEMICOLON, KC_P, KC_BSLS, KC_SLASH, KC_RBRC, KC_NO,
+        KC_F, KC_R, KC_G, KC_V, LSFT(KC_QUOTE), KC_NO,
+        KC_D, KC_E, KC_T, KC_C, KC_GRAVE, KC_NO,
+        KC_S, KC_W, KC_B, KC_X, KC_ESCAPE, KC_NO,
+        KC_A, KC_Q, KC_LBRC, KC_Z, KC_DELETE, KC_NO,
+        MO(NAVNAS), KC_SPACE, KC_NO, KC_BSPC, KC_LALT, MO(FUNC),
+        KC_LSFT, LT(NAVNAS, KC_ENTER), KC_NO, LGUI_T(KC_TAB), KC_LCTL, KC_CAPS       
+    ),
 
     [NAVNAS] = LAYOUT(
-        /*     Center            North               East               South             West              Double*/
-        /*R1*/ KC_7              , LSFT(KC_7)        , LSFT(KC_6)       , KC_LEFT         , KC_6            , KC_NO           ,
-        /*R2*/ KC_8              , LSFT(KC_8)        , KC_NO            , KC_UP           , LSFT(KC_MINUS)  , KC_NO           ,
-        /*R3*/ KC_9              , LSFT(KC_9)        , KC_NO            , KC_DOWN         , KC_INSERT       , KC_NO           ,
-        /*R4*/ KC_0              , LSFT(KC_0)        , KC_NO            , KC_RIGHT        , LSFT(KC_GRAVE)  , KC_NO           ,
-        /*L1*/ KC_4              , LSFT(KC_4)        , KC_5             , KC_END          , LSFT(KC_5)      , KC_NO           ,
-        /*L2*/ KC_3              , LSFT(KC_3)        , KC_MINUS         , KC_PGDN       , LSFT(KC_EQUAL)  , KC_NO           ,
-        /*L3*/ KC_2              , LSFT(KC_2)        , KC_DOT           , KC_PGUP         , KC_TRNS         , KC_NO           ,
-        /*L4*/ KC_1              , LSFT(KC_1)        , KC_EQUAL         , KC_HOME         , KC_TRNS         , KC_NO           ,
-        /*Down             Pad             Up              Nail            Knuckle         DoubleDown*/
-        /*RT*/ KC_TRNS           , KC_TRNS           , KC_TRNS          , KC_TRNS         , KC_TRNS         , KC_TRNS         ,
-        /*LT*/ KC_TRNS           , KC_TRNS           , KC_TRNS          , KC_TRNS         , KC_TRNS         , KC_TRNS         
-        ),
+        KC_7, LSFT(KC_7), LSFT(KC_6), KC_LEFT, KC_6, KC_NO,
+        KC_8, LSFT(KC_8), KC_NO, KC_UP, LSFT(KC_MINUS), KC_NO,
+        KC_9, LSFT(KC_9), KC_NO, KC_DOWN, KC_INSERT, KC_NO,
+        KC_0, LSFT(KC_0), KC_NO, KC_RIGHT, LSFT(KC_GRAVE), KC_NO,
+        KC_4, LSFT(KC_4), KC_5, KC_END, LSFT(KC_5), KC_NO,
+        KC_3, LSFT(KC_3), KC_MINUS, KC_PGDN, LSFT(KC_EQUAL), KC_NO,
+        KC_2, LSFT(KC_2), KC_DOT, KC_PGUP, KC_TRNS, KC_NO,
+        KC_1, LSFT(KC_1), KC_EQUAL, KC_HOME, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
+    ),
     
-    /* ===== FUNCTION KEYS ===== */
     [FUNC] = LAYOUT(
-        /*Center           North           East            South           West            Double*/
-        /*R1*/ KC_F7            , KC_NO             , KC_F16           , KC_F17         , KC_F6           , KC_NO           ,
-        /*R2*/ KC_F8            , KC_NO             , KC_NO            , KC_F18         , KC_NO           , KC_NO           ,
-        /*R3*/ KC_F9            , KC_NO             , KC_NO            , KC_F19         , KC_NO           , KC_NO           ,
-        /*R4*/ KC_F10           , KC_NO             , KC_NO            , KC_F20         , KC_NO           , KC_NO           ,
-        /*L1*/ KC_F4            , KC_F24            , KC_F5            , KC_F14         , KC_F15          , KC_NO           ,
-        /*L2*/ KC_F3            , KC_F23            , KC_F10           , KC_F13         , KC_NO           , KC_NO           ,
-        /*L3*/ KC_F2            , KC_F22            , KC_NO            , KC_F12         , KC_NO           , KC_NO           ,
-        /*L4*/ KC_F1            , KC_F21            , KC_NO            , KC_F11         , KC_NO           , KC_NO           ,
-        /*Down             Pad             Up              Nail            Knuckle         DoubleDown*/
-        /*RT*/ KC_TRNS          , KC_TRNS           , KC_TRNS          , KC_TRNS        , KC_TRNS         , KC_TRNS         ,
-        /*LT*/ KC_TRNS          , KC_TRNS           , KC_TRNS          , KC_TRNS        , KC_TRNS         , KC_TRNS         
-        ),
+        KC_F7, KC_NO, KC_F16, KC_F17, KC_F6, KC_NO,
+        KC_F8, KC_NO, KC_NO, KC_F18, KC_NO, KC_NO,
+        KC_F9, KC_NO, KC_NO, KC_F19, KC_NO, KC_NO,
+        KC_F10, KC_NO, KC_NO, KC_F20, KC_NO, KC_NO,
+        KC_F4, KC_F24, KC_F5, KC_F14, KC_F15, KC_NO,
+        KC_F3, KC_F23, KC_F10, KC_F13, KC_NO, KC_NO,
+        KC_F2, KC_F22, KC_NO, KC_F12, KC_NO, KC_NO,
+        KC_F1, KC_F21, KC_NO, KC_F11, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
+    ),
 
     [BOARD_CONFIG] = LAYOUT(
-        /*         Center              North               East                South               West                (XXX)               */
-        /*R1*/     KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_NO,
-        /*R2*/     KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_NO,
-        /*R3*/     KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_NO,
-        /*R4*/     KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_NO,
-        /*L1*/     SV_OUTPUT_STATUS,   KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_NO,
-        /*L2*/     KC_TRNS,            SV_RIGHT_DPI_INC,   KC_TRNS,            SV_RIGHT_DPI_DEC,   KC_TRNS,            KC_NO,
-        /*L3*/     KC_TRNS,            SV_LEFT_DPI_INC,    KC_TRNS,            SV_LEFT_DPI_DEC,    KC_TRNS,            KC_NO,
-        /*L4*/     KC_TRNS,            KC_TRNS, KC_TRNS,   KC_TRNS,            KC_TRNS,            KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        SV_OUTPUT_STATUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, SV_RIGHT_DPI_INC, KC_TRNS, SV_RIGHT_DPI_DEC, KC_TRNS, KC_NO,
+        KC_TRNS, SV_LEFT_DPI_INC, KC_TRNS, SV_LEFT_DPI_DEC, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    ),
 
-        /*        Down                Pad                 Up                  Nail                Knuckle             Double Down         */
-        /* RT */  KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,
-        /* LT */  KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS,            KC_TRNS
-      ),
-
-    /* ===== MBO ===== */
     [MBO] = LAYOUT(
-        /*      Center           North               East                South                West                Double*/
-        /*R1*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*R2*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*R3*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*R4*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*L1*/ KC_BTN1           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*L2*/ KC_BTN3           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*L3*/ KC_BTN2           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_NO             ,
-        /*L4*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , SV_SNIPER_3       , KC_TRNS           , KC_NO             ,
-        
-        /*     Down               Pad                Up                  Nail                Knuckle             DoubleDown */
-        /*RT*/ KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           , KC_TRNS           ,
-        /*LT*/ KC_TRNS           , KC_BTN1           , KC_TRNS           , KC_BTN2           , KC_TRNS           , KC_TRNS          
-        ),
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, SV_SNIPER_3, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_BTN1, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS          
+    ),
 };
 #endif
 
 void keyboard_post_init_user(void) {
-  // Customise these values if you need to debug the matrix
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-
 #if __has_include("keymap_all.h")
   if (fresh_install) {
     sval_init_defaults();
   } 
+#endif
 }
 
 bool is_jiggling = false;
@@ -152,13 +114,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_F24:
             if (record->event.pressed) {
                 is_jiggling = !is_jiggling;
-                
                 if (is_jiggling) {
-                    layer_on(5);
+                    layer_on(MBO);
                     jiggle_timer = timer_read32();
                     jiggle_step = 0;
                 } else {
-                    layer_off(5);
+                    layer_move(NORMAL);
                 }
             }
             return false;
@@ -168,12 +129,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void matrix_scan_user(void) {
     if (!is_jiggling) return;
-
     if (jiggle_step == 0 && timer_elapsed32(jiggle_timer) > 30000) {
         report_mouse_t report = {0};
         report.y = 50; 
         host_mouse_send(&report);
-        
         jiggle_step = 1;
         jiggle_timer = timer_read32();
     }
@@ -181,7 +140,6 @@ void matrix_scan_user(void) {
         report_mouse_t report = {0};
         report.y = -50; 
         host_mouse_send(&report);
-        
         jiggle_step = 2; 
         jiggle_timer = timer_read32();
     }
@@ -189,9 +147,15 @@ void matrix_scan_user(void) {
         report_mouse_t report = {0};
         report.y = 0; 
         host_mouse_send(&report);
-        
         jiggle_step = 0;
         jiggle_timer = timer_read32();
     }
 }
 
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (is_jiggling && (mouse_report.x != 0 || mouse_report.y != 0)) {
+        is_jiggling = false;
+        layer_move(NORMAL);
+    }
+    return mouse_report;
+}
