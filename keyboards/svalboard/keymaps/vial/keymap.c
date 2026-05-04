6263ae643a22488e7265848e2bfc705cb8a83380
Efrain Saldana <43555262+epsldn@users.noreply.github.com>
2026-05-04 17:22:23 -0500

    Update keymap.c

--- keyboards/svalboard/keymaps/vial/keymap.c
+++ keyboards/svalboard/keymaps/vial/keymap.c
@@ -5,6 +5,9 @@
 #include <stdint.h>
 #include "svalboard.h"
 
+report_mouse_t support_pointing_device_task_user(report_mouse_t mouse_report);
+void support_keyboard_post_init_user(void);
+
 #define pointing_device_task_user support_pointing_device_task_user
 #define keyboard_post_init_user support_keyboard_post_init_user
 #include "../keymap_support.c"
@@ -39,59 +42,55 @@ const uint16_t PROGMEM keymaps[DYNAMIC_KEYMAP_LAYER_COUNT][MATRIX_ROWS][MATRIX_C
         KC_S, KC_W, KC_B, KC_X, KC_ESCAPE, KC_NO,
         KC_A, KC_Q, KC_LBRC, KC_Z, KC_DELETE, KC_NO,
         MO(NAVNAS), KC_SPACE, KC_NO, KC_BSPC, KC_LALT, MO(FUNC),
-        KC_LSFT, LT(NAVNAS, KC_ENTER), KC_NO, LGui_T(KC_TAB), KC_LCTL, KC_CAPS       
+        KC_LSFT, LT(NAVNAS, KC_ENTER), KC_NO, LGUI_T(KC_TAB), KC_LCTL, KC_CAPS       
     ),
-
     [NAVNAS] = LAYOUT(
-        /*R1*/ KC_7, LSFT(KC_7), LSFT(KC_6), KC_LEFT, KC_6, KC_NO,
-        /*R2*/ KC_8, LSFT(KC_8), KC_NO, KC_UP, LSFT(KC_MINUS), KC_NO,
-        /*R3*/ KC_9, LSFT(KC_9), KC_NO, KC_DOWN, KC_INSERT, KC_NO,
-        /*R4*/ KC_0, LSFT(KC_0), KC_NO, KC_RIGHT, LSFT(KC_GRAVE), KC_NO,
-        /*L1*/ KC_4, LSFT(KC_4), KC_5, KC_END, LSFT(KC_5), KC_NO,
-        /*L2*/ KC_3, LSFT(KC_3), KC_MINUS, KC_PGDN, LSFT(KC_EQUAL), KC_NO,
-        /*L3*/ KC_2, LSFT(KC_2), KC_DOT, KC_PGUP, KC_TRNS, KC_NO,
-        /*L4*/ KC_1, LSFT(KC_1), KC_EQUAL, KC_HOME, KC_TRNS, KC_NO,
-        /*RT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        /*LT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
+        KC_7, LSFT(KC_7), LSFT(KC_6), KC_LEFT, KC_6, KC_NO,
+        KC_8, LSFT(KC_8), KC_NO, KC_UP, LSFT(KC_MINUS), KC_NO,
+        KC_9, LSFT(KC_9), KC_NO, KC_DOWN, KC_INSERT, KC_NO,
+        KC_0, LSFT(KC_0), KC_NO, KC_RIGHT, LSFT(KC_GRAVE), KC_NO,
+        KC_4, LSFT(KC_4), KC_5, KC_END, LSFT(KC_5), KC_NO,
+        KC_3, LSFT(KC_3), KC_MINUS, KC_PGDN, LSFT(KC_EQUAL), KC_NO,
+        KC_2, LSFT(KC_2), KC_DOT, KC_PGUP, KC_TRNS, KC_NO,
+        KC_1, LSFT(KC_1), KC_EQUAL, KC_HOME, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
     ),
-    
     [FUNC] = LAYOUT(
-        /*R1*/ KC_F7, KC_NO, KC_F16, KC_F17, KC_F6, KC_NO,
-        /*R2*/ KC_F8, KC_NO, KC_NO, KC_F18, KC_NO, KC_NO,
-        /*R3*/ KC_F9, KC_NO, KC_NO, KC_F19, KC_NO, KC_NO,
-        /*R4*/ KC_F10, KC_NO, KC_NO, KC_F20, KC_NO, KC_NO,
-        /*L1*/ KC_F4, KC_F24, KC_F5, KC_F14, KC_F15, KC_NO,
-        /*L2*/ KC_F3, KC_F23, KC_F10, KC_F13, KC_NO, KC_NO,
-        /*L3*/ KC_F2, KC_F22, KC_NO, KC_F12, KC_NO, KC_NO,
-        /*L4*/ KC_F1, KC_F21, KC_NO, KC_F11, KC_NO, KC_NO,
-        /*RT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        /*LT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
+        KC_F7, KC_NO, KC_F16, KC_F17, KC_F6, KC_NO,
+        KC_F8, KC_NO, KC_NO, KC_F18, KC_NO, KC_NO,
+        KC_F9, KC_NO, KC_NO, KC_F19, KC_NO, KC_NO,
+        KC_F10, KC_NO, KC_NO, KC_F20, KC_NO, KC_NO,
+        KC_F4, KC_F24, KC_F5, KC_F14, KC_F15, KC_NO,
+        KC_F3, KC_F23, KC_F10, KC_F13, KC_NO, KC_NO,
+        KC_F2, KC_F22, KC_NO, KC_F12, KC_NO, KC_NO,
+        KC_F1, KC_F21, KC_NO, KC_F11, KC_NO, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS         
     ),
-
     [BOARD_CONFIG] = LAYOUT(
-        /*R1*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R2*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R3*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R4*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L1*/ SV_OUTPUT_STATUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L2*/ KC_TRNS, SV_RIGHT_DPI_INC, KC_TRNS, SV_RIGHT_DPI_DEC, KC_TRNS, KC_NO,
-        /*L3*/ KC_TRNS, SV_LEFT_DPI_INC, KC_TRNS, SV_LEFT_DPI_DEC, KC_TRNS, KC_NO,
-        /*L4*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*RT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        /*LT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        SV_OUTPUT_STATUS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, SV_RIGHT_DPI_INC, KC_TRNS, SV_RIGHT_DPI_DEC, KC_TRNS, KC_NO,
+        KC_TRNS, SV_LEFT_DPI_INC, KC_TRNS, SV_LEFT_DPI_DEC, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
-
     [MBO] = LAYOUT(
-        /*R1*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R2*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R3*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*R4*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L1*/ KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L2*/ KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L3*/ KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
-        /*L4*/ KC_TRNS, KC_TRNS, KC_TRNS, SV_SNIPER_3, KC_TRNS, KC_NO,
-        /*RT*/ KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        /*LT*/ KC_TRNS, KC_BTN1, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS          
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN1, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, SV_SNIPER_3, KC_TRNS, KC_NO,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_BTN1, KC_TRNS, KC_BTN2, KC_TRNS, KC_TRNS          
     ),
 };
 #endif
@@ -130,7 +129,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void matrix_scan_user(void) {
     if (!is_jiggling) return;
-    if (jiggle_step == 0 && timer_elapsed32(jiggle_timer) > 30000) {
+    if (jiggle_step == 0 && timer_elapsed32(jiggle_timer) > 15000) {
         report_mouse_t report = {0};
         report.y = 50; 
         host_mouse_send(&report);
